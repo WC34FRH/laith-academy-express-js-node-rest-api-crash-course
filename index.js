0ea90c62c0d4e3de75080ea6710fb56b8927855b
Zander Ellis <zander.ellis@wearephlo.com>
2022-08-30 17:57:54 +0100

    tutorial completed up to 10:52

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
+const todos = [];
+
+app.get("/todos", (req, res) => {
+  return res.json(todos);
+});
+
+app.post("todos");
+
 app.listen(3000, () => {
   console.log("Now listening on port 3000");
 });
