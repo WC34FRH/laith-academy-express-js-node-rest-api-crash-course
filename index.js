6e31588e234f13c43d52365582048c3ef4573c59
Zander Ellis <zander.ellis@wearephlo.com>
2022-08-31 11:36:12 +0100

    issue encountered at 16:46

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { v4 } = require("uuid");
 
 const app = express();
+app.use(express.json);
 
 const todos = [];
 
@@ -8,7 +10,16 @@ app.get("/todos", (req, res) => {
   return res.json(todos);
 });
 
-app.post("todos");
+app.post("/todos", (req, res) => {
+  const { title } = req.body;
+  const todo = {
+    id: v4(),
+    title,
+    completed: false,
+  };
+  todos.push(todo);
+  return res.json(todo);
+});
 
 app.listen(3000, () => {
   console.log("Now listening on port 3000");
